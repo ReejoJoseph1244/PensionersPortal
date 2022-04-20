5568067ae3ae488db290c7c2aa2450b3c71d1d2b
Reejo Joseph <92742868+ReejoJoseph1244@users.noreply.github.com>
2022-04-20 09:37:24 +0530

    Delete admin.py

--- assets/HomePage/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from HomePage.models import NSAP_deatils , CENSUS_Detail
-# Register your models here.
-admin.site.register(NSAP_deatils)
-admin.site.register(CENSUS_Detail)
